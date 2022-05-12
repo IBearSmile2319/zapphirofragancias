e8328009bdb6f83ec50cb613c00bc781e943927a
Maicol Manuel Orosco Vasquez <78711486+IBearSmile2319@users.noreply.github.com>
2022-05-11 21:14:24 -0500

    sign in zf socios

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
+import './assets/css/IBearSmile.css'
 window.store=store
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
