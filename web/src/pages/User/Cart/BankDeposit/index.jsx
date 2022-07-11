93d805126488ab77810e42761dcd02c3aac5783c
Maicol Manuel Orosco Vasquez <78711486+IBearSmile2319@users.noreply.github.com>
2022-07-10 21:58:34 -0500

    bank

--- web/src/pages/User/Cart/BankDeposit/index.jsx
+++ web/src/pages/User/Cart/BankDeposit/index.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-
+import { LeftOutlined } from '@ant-design/icons'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import checkPayment from '../../../../helpers/PaymentMethod'
 const BankDeposit = () => {
+  const navigate = useNavigate()
+  const [paymentMethod, setPaymentMethod] = useState(null)
+  useEffect(() => {
+    const paymentName = localStorage.getItem('paymentMethod')
+    if (paymentName) {
+      checkPayment.map((item, index) => {
+        if (item.name === paymentName) {
+          setPaymentMethod(item)
+        }
+      })
+    } else {
+      navigate('/cart')
+    }
+  }, [])
   return (
-    <div>
-      deposito bancario
-    </div>
+    <>
+      <header className="cart-payment-header">
+        <div className="payment-header-content"
+          onClick={() => navigate('/cart')}
+        >
+          <p>
+            <Link to="/cart">
+              <LeftOutlined />
+            </Link>
+            {
+              paymentMethod ? paymentMethod.name : 'Selecciona un método de pago'
+            }</p>
+          {
+            paymentMethod ?
+              <img src={paymentMethod.icon} alt="paymentMethod" />
+              :
+              <img src="https://app.ed.team/static/images/utils/edy/edy-shopping.svg" alt="shopping" />
+          }
+        </div>
+      </header>
+    </>
   )
 }
 
--- web/src/pages/User/Cart/PaymentMethod/index.jsx
+++ web/src/pages/User/Cart/PaymentMethod/index.jsx
@@ -1,44 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import checkPayment from '../../../../helpers/PaymentMethod'
 import './PaymentMethod.css'
-// const checkPayment = [
-//   {
-//     name: 'Efectivo',
-//     person: 'ZAPPHIRO COMPANY',
-//     number: 'Pago en Tienda',
-//   },
-//   {
-//     name: 'YAPE',
-//     person: "IRENIA SABINA GARAY LAZO",
-//     number: "951-762-095",
-//   },
-//   {
-//     name: 'PLIN',
-//     person: "RONALD JHUVER MONTES",
-//     number: "980-525-605",
-//   },
-//   {
-//     name: 'INTERBANK CTA, CTE',
-//     person: "ZAPPHIRO COMPANY",
-//     number: "011-3003042621"
-//   },
-//   {
-//     name: 'BBVA AHORROS',
-//     person: "RONALD JHUVER MONTES",
-//     number: "0011-0169-020036583",
-//   },
-//   {
-//     name: 'BCP AHORROS',
-//     person: "IRENIA SABINA GARAY LAZO",
-//     number: "194-30044964047",
-//   },
-//   {
-//     name: 'MI BANCO AHORROS',
-//     person: "IRENIA SABINA GARAY LAZO",
-//     number: "6008866447",
-//   }
-// ]
+import { useNavigate } from 'react-router-dom'
 const PaymentMethod = () => {
+  const navigate = useNavigate()
+  const handleChange = (value) => {
+    localStorage.setItem('paymentMethod', value)
+    navigate('/cart/deposit')
+  }
   return (
     <>
       <header className="cart-payment-header">
@@ -49,7 +18,9 @@ const PaymentMethod = () => {
       <ul className="payment-method-list">
         {
           checkPayment.map((item, index) => (
-            <li className="payment-item" key={index}>
+            <li className="payment-item" key={index}
+              onClick={() => handleChange(item.name)}
+            >
               <p>
                 {item.name}
               </p>
