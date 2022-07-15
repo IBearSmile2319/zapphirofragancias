f18147fe91cf370253473839330eb3a0c8b9b908
Maicol Manuel Orosco Vasquez <78711486+IBearSmile2319@users.noreply.github.com>
2022-07-15 08:06:39 -0500

    affiliate user

--- web/src/pages/User/Affiliates/index.jsx
+++ web/src/pages/User/Affiliates/index.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
-
+import './Affiliates.css'
+import FeaturedUser from './FeaturedUser'
 const Affiliates = () => {
   return (
-    <div>
-      afiliados
-    </div>
+    <>
+      <div style={{
+        flexGrow: 0,
+        marginBottom: '2rem',
+        marginTop: 0,
+      }}></div>
+      <div className="affiliate-main">
+        <div className="affiliate-list">
+          <div>
+            <section className="list-group">
+              <div className="zf-grid">
+                <div className="comunity-title">
+                  <b className="title">
+                    Mis afiliados
+                  </b>
+                </div>
+                <div className="featured-user-content">
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                  <FeaturedUser />
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
